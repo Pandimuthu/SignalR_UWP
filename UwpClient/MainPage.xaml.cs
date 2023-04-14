dd2c5345df761e079caee13e6bcfd35f81cc0eed
Pandimuthu <muthupandi95.mp@gmail.com>
2023-04-14 16:58:18 +0530

    Add files via upload

---
+++ UwpClient/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Microsoft.AspNet.SignalR.Client;
+using System.ComponentModel;
+using Windows.UI.Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpClient
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        int count = 0;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            MessageList = new ObservableCollection<string>();
+            InitilizeHub();
+        }
+
+        public ObservableCollection<string> MessageList
+        {
+            get;
+            set;
+        }
+
+        private async void InitilizeHub()
+        {
+            var hubConnection = new HubConnection("http://localhost:50869");
+            var hubProxy = hubConnection.CreateHubProxy("BroadcastHub");
+
+            hubProxy.On<string>("Broadcast",
+                                  async data =>
+                                  await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                                                                          () =>
+                                                                          {
+                                                                              count=count+1;
+                                                                              BroadcastResults.Text = count.ToString();
+                                                                              MessageList.Add(data);
+                                                                              //notiifcationList.ItemsSource = messageList;
+                                                                          }
+                                                                          ));
+            await hubConnection.Start();
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void message(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UwpClient/UserControls/Post.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace InstagramDesktopExam.UserControls
+{
+   
+    public partial class Post : UserControl
+    {
+        public Post()
+        {
+            InitializeComponent();
+        }
+
+        public ImageSource PostSource
+        {
+            get { return (ImageSource)GetValue(PostSourceProperty); }
+            set { SetValue(PostSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty PostSourceProperty = DependencyProperty.Register("PostSource", typeof(ImageSource), typeof(Post), new PropertyMetadata(null));
+
+        public ImageSource ProfileSource
+        {
+            get { return (ImageSource)GetValue(ProfileSourceProperty); }
+            set { SetValue(ProfileSourceProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileSourceProperty = DependencyProperty.Register("ProfileSource", typeof(ImageSource), typeof(Post), new PropertyMetadata(null));
+
+        public string ProfileName
+        {
+            get { return (string)GetValue(ProfileNameProperty); }
+            set { SetValue(ProfileNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileNameProperty = DependencyProperty.Register("ProfileName", typeof(string), typeof(Post), new PropertyMetadata(null));
+
+        public string Likes
+        {
+            get { return (string)GetValue(LikesProperty); }
+            set { SetValue(LikesProperty, value); }
+        }
+
+        public static readonly DependencyProperty LikesProperty = DependencyProperty.Register("Likes", typeof(string), typeof(Post), new PropertyMetadata(null));
+
+        public string Comments
+        {
+            get { return (string)GetValue(CommentsProperty); }
+            set { SetValue(CommentsProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommentsProperty = DependencyProperty.Register("Comments", typeof(string), typeof(Post), new PropertyMetadata(null));
+
+
+
+
+
+
+    }
+}
